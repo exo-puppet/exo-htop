5340a165839ff2bf321df35c4c3ab1192724363d
Frederic DROUET <fdrouet@exoplatform.com>
2011-11-17 18:29:41 +0100

    SWF-1609 : the htop package is now configurable to choose if it should only be present or always present and up-todate

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,11 @@
-class htop {
-	include htop::install
+# Class: htop
+#
+# This class manage htop
+# 
+# Parameters:
+#	$lastversion:
+#		this variable allow to chose if the htop package should always be updated (true) or not (false) (default: false)
+class htop ($lastversion = "false") {
+#	include htop::install
+	class { "htop::install": }
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,14 @@
+# Class: htop::install
+#
+# This class manage the installation of the htop package
 class htop::install {
-	package { "htop" :
-		ensure => present
+	
+	$htop_ensure = $htop::lastversion ? {
+		true => latest,
+		default => present
 	}
+	#notify { "htop ensure mode = $htop_ensure": withpath => false }
+	info ("htop ensure mode = $htop_ensure")
+	
+	package { "htop" : ensure => $htop_ensure }
 }
