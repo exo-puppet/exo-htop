e0dfc653cf99931a4ca09a6f143c88c205cb1cb4
Frederic DROUET <fdrouet@exoplatform.com>
2011-12-07 18:12:31 +0100

    SWF-1662 : htop module use the repo puppet module + clean doc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,33 @@
-# Class: htop
+################################################################################
 #
 # This class manage htop
-# 
-# Parameters:
-#	$lastversion:
-#		this variable allow to chose if the htop package should always be updated (true) or not (false) (default: false)
+#
+# Tested platforms:
+#   - Ubuntu 11.10 Oneiric
+#   - Ubuntu 11.04 Natty
+#   - Ubuntu 10.04 Lucid
+#
+# == Parameters
+#
+#   [+lastversion+]
+#       (OPTIONAL) (default: false)
+#       
+#       this variable allow to chose if the htop package should always be updated (true) or not (false)
+#
+# == Modules Dependencies
+#
+# [+repo+]
+#   the +repo+ puppet module is needed to :
+#   
+#   - refresh the repository before installing package (in htop::install)
+#
+# == Examples
+#
+#   class { "htop": lastversion => false }
+#
+################################################################################
 class htop ($lastversion = "false") {
-#	include htop::install
-	class { "htop::install": }
+    
+    include repo
+    include htop::install
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -4,11 +4,12 @@
 class htop::install {
 	
 	$htop_ensure = $htop::lastversion ? {
-		true => latest,
-		default => present
+		true      => latest,
+		default   => present
 	}
-	#notify { "htop ensure mode = $htop_ensure": withpath => false }
-	info ("htop ensure mode = $htop_ensure")
 	
-	package { "htop" : ensure => $htop_ensure }
+    package { "htop" : 
+	   ensure  => $htop_ensure,
+	   require => Exec ["repo-update"],
+    }
 }
